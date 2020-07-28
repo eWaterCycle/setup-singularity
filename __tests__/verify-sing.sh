192aad3bd7d97de24cbafced938904301e6d461c
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-07-28 12:25:38 +0200

    Fix verify

--- __tests__/verify-sing.sh
+++ __tests__/verify-sing.sh
@@ -6,8 +6,8 @@ if [ -z "$1" ]; then
 fi
 
 sing_version="$(singularity --version)"
-echo "Found singularity version '$sing_version'"
-if [ -z "$(echo $sing_version | grep $3)" ]; then
+echo "Found '$sing_version'"
+if [ -z "$(echo singularity version $sing_version | grep $1)" ]; then
   echo "Unexpected version"
   exit 1
 fi
